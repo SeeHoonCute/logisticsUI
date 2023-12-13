22e5b166371fc5d59435cba7aa7399ec32c9d244
hipnh <hipnhdev@gmail.com>
2023-12-13 21:02:40 +0700

    fix table

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -15,7 +15,7 @@ import IconButton from '@mui/material/IconButton';
 import Tooltip from '@mui/material/Tooltip';
 import { Table } from "@mui/material";
 
-interface IRouteType {
+export interface IRouteType {
     id: number;
     routeId: string;
     warehouse: string;
